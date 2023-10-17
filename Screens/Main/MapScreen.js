d32fc132ebfe80944f0e18c8f2cf277c47dbfa67
Khavkin <hdm@ukr.net>
2023-10-17 21:09:57 +0300

    add mapview component

--- Screens/Main/MapScreen.js
+++ Screens/Main/MapScreen.js
@@ -1,18 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, Dimensions } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
-export const MapScreen = () => {
-  return <View style={styles.mapContainer}></View>;
+const MapScreen = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.mapStyle}
+        region={{
+          latitude: 47.83346,
+          longitude: 35.13582,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        mapType="standard"
+        minZoomLevel={15}
+        onMapReady={() => console.log("Map is ready")}
+        onRegionChange={() => console.log("Region change")}
+      >
+        <Marker
+          draggable
+          title="I am here"
+          coordinate={{ latitude: 47.83346, longitude: 35.13582 }}
+          description="Hello"
+        />
+      </MapView>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-  mapContainer: {
-    // width: "100%",
-    // heigth: "100%",
-    backgroundColor: "#E8E8E8",
+  container: {
     flex: 1,
-    borderWidth: 2,
-    justifyContent: "center",
+    backgroundColor: "#fff",
     alignItems: "center",
+    justifyContent: "center",
+  },
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
 
